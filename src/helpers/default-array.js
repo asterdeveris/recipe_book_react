2972b43e90eeef3def73317cf9780d5000a5109b
Dasha <asterdeveris@gmail.com>
2023-03-09 16:53:14 +0200

    add scss

---
+++ src/helpers/default-array.js
@@ -0,0 +1,66 @@
+const defaultArrayOfRecepes = [
+  {
+    name: "Cheese Pancakes with Apple (Syrniki)",
+    ingridients: [
+      "recipe from https://natashaskitchen.com/",
+      "2 cups farmers cheese, homemade or store-bought",
+      "4 large eggs",
+      "3/4 cups all-purpose flour + 1/2 cup for dredging",
+      "3 Tbsp granulated sugar",
+      "1/2 tsp salt",
+      "1/2 tsp cinnamon",
+      "1 tsp baking soda",
+      "1 tsp white vinegar",
+      "1 medium apple (any variety), peeled, cored and finely diced (about 1 1/2 cups)",
+      "Light olive oil, canola or vegetable oil, to sautee",
+    ],
+    directions: [
+      " In a large bowl combine 2 cups farmers cheese, 4 eggs, 3/4 cup flour, 3 Tbsp sugar, 1/2 tsp salt and 1/2 tsp cinnamon. Use an electric hand mixer on medium speed and mix just until well blended.",
+      "Place 1 tsp baking soda into a small ramekin and add 1 tsp vinegar then stir to combine. It should fizz. Pour this mixture into the batter and mix until well blended. Use a spatula to fold in diced apples. The mixture will thicken slightly as it stands.",
+      "Heat a large non-stick pan (this Zwilling pan is the best one I’ve ever tried) over medium/low heat and add 2 to 3 Tbsp oil.",
+      "Fill a shallow bowl with 1/2 cup flour. Add batter a heaping tablespoon at a time (I used a flat ice cream scoop with trigger). Sprinkle flour generously over the top then gently lift pancake and pat from one hand to another to remove excess flour.",
+      "Saute in hot oil over med/low heat about 3 min per side. Flip when starting to form tiny bubbles on top. Transfer to a platter and serve warm with your favorite topping such as jam and sour cream!",
+    ],
+  },
+  {
+    name: "Galushki (Ukrainian Dumplings)",
+    ingridients: [
+      "recipe from recipesfromeurope.com",
+      "2/3 cup all-purpose flour",
+      "1/2 teaspoon salt, more for salting water",
+      "1 egg",
+      "6 tablespoons milk",
+      "2 cups mushrooms, chopped",
+      "1 cup yellow onion, chopped",
+      "1 tablespoon olive oil",
+      "1 tablespoon butter",
+    ],
+    directions: [
+      "Add the flour to a deep mixing bowl. Then make a well in the center, add the salt and beat in an egg.",
+      "Now add milk. Mix first with a spoon and then with your hands until you have a soft dough. Cover the bowl with cling film and let it sit for 20 minutes.",
+      "Meanwhile, add the chopped mushrooms and onions to a frying pan with olive oil and a pinch of salt. Fry the mushrooms and onions on low heat for 15 minutes while stirring regularly. Set aside.",
+      "Sprinkle your work surface with a bit of flour so that the dough does not stick. Place the dough onto the surface and cut it into quarters. Roll one quarter of dough into a thin strip (like a sausage), about half an inch in diameter. Cut the strip into thin slices about a third of an inch thick. Repeat with the other pieces of dough.",
+      "Add water to a large pot, bring it to a boil and salt it. Then add the pieces of dough to the water. Once they float to the surface, cook them for 3 more minutes.",
+      "Remove the finished dumplings with a slotted spoon and transfer them to a bowl. Add the butter and stir.",
+      "When the butter has melted, add the fried mushrooms and onions to the bowl with the dumplings as well. Stir again. Now your Galushki are ready to be served. You can serve them with sour cream. Enjoy!",
+    ],
+  },
+  {
+    name: "Classic crêpes",
+    ingridients: [
+      "recipe by Good Food team",
+      "140g plain flour",
+      "200ml whole milk",
+      "2 eggs",
+      "25g unsalted butter, melted, plus a little extra for greasing",
+    ],
+    directions: [
+      "Sift the flour with a pinch of salt into a medium-size bowl and make a well in the middle. Mix the milk and 100ml of water together. Break the eggs into the well and start whisking slowly. Add the milk and water in a steady stream, whisking constantly and gradually incorporating the flour as you do so.",
+      "Whisk until the batter is smooth and all the flour has been incorporated. Set the batter aside to rest for 30 mins, then whisk the melted butter into the batter.",
+      "Heat the pan over a medium heat. Very lightly grease the pan with melted butter. Using a ladle, pour roughly 2 tbsp of batter into the pan and swirl it around so the bottom of the pan is evenly coated. You want to use just enough batter to make a delicate, lacy pancake. Cook the pancake for about 45 secs on one side until golden and then using a palette knife or fish slice, flip the pancake over and cook the other side for about 30 secs until it freckles.",
+      "Slide the pancake out of the pan and either serve immediately or stack on a plate with baking parchment in between. Continue until all the batter is used up.",
+    ],
+  },
+];
+
+export default defaultArrayOfRecepes;
